01978c7f115c433c24df9cbf1247b964fae4450f
u-san <u_san@foxmail.com>
2017-02-16 01:00:59 +0800

    add

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ const path =require('path');
 
 module.exports = {
     BASE_GIT_PATH: 'git+https://github.com/u-san/usan-cli.git',
-    FRAMEWORK_PATH: path.join(__dirname, '../framework')
+    FRAMEWORK_PATH: path.join(__dirname, '../frame')
 }
